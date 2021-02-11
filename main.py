408328a9f9b066733b0e90fb5722a65300159f76
petercolella <peter.joseph.colella@gmail.com>
2021-02-10 23:24:34 -0500

    16 - 20

---
+++ main.py
@@ -0,0 +1,26 @@
+#Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized Data Types
+
+None
+
+print(2 + 4)
+print(type(2 + 4))
+print(type(2 / 4))
+print(2 ** 4)
+print(9 // 4)
+print(9 % 4)
+
+#math functions
+print(round(3.9))
+print(abs(-20))
