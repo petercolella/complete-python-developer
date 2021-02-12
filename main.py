68d352c77754e2bcb3762dee54c1e52608f529dd
petercolella <peter.joseph.colella@gmail.com>
2021-02-11 22:01:33 -0500

    26 - 31

--- main.py
+++ main.py
@@ -36,3 +36,42 @@ print(20 - 3 * 4)
 complex
 print(bin(5))
 print(int('0b101', 2))
+
+# augmented assignment operator
+some_value = 5
+# some_value = some_value + 2
+some_value += 2
+
+print(some_value)
+
+print(type('Hello world!'))
+long_string = '''
+WOW
+0 0
+---
+'''
+
+print(long_string)
+
+first_name = 'Peter'
+last_name = 'Colella'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+# string concatenation
+print('hello' + ' Peter')
+# print('hello' + 5)
+
+# type conversion
+print(type(int(str(100))))
+
+a = str(100)
+b = int(a)
+c = type(b)
+print(c)
+
+# Escape Sequence
+# weather = 'It\'s \'kind of\' sunny'
+weather = '\t It\'s \'kind of\' sunny \n have a good day!'
+
+print(weather)
