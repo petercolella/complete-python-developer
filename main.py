b5d2d3ab7325447257b190ac8a99bf609810f22f
petercolella <peter.joseph.colella@gmail.com>
2021-02-18 21:13:44 -0500

    48 - 57

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Fundamental Data Types
+# Fundamental Data Types
 int
 float
 bool
@@ -8,9 +8,9 @@ tuple
 set
 dict
 
-#Classes -> custom types
+# Classes -> custom types
 
-#Specialized Data Types
+# Specialized Data Types
 
 None
 
@@ -21,11 +21,11 @@ print(2 ** 4)
 print(9 // 4)
 print(9 % 4)
 
-#math functions
+# math functions
 print(round(3.9))
 print(abs(-20))
 
-#operator precedence
+# operator precedence
 print(20 - 3 * 4)
 
 # ()
@@ -86,7 +86,8 @@ print(f'hi {name} You are {age} years old')
 print('hi {}. You are {} years old'.format('Johnny', 55))
 print('hi {}. You are {} years old'.format(name, age))
 print('hi {1}. You are {0} years old'.format(name, age))
-print('hi {new_name}. You are {age} years old'.format(new_name='sally', age=100))
+print('hi {new_name}. You are {age} years old'.format(
+    new_name='sally', age=100))
 
 selfish = 'me me me'
 nums = '01234567'
@@ -150,18 +151,18 @@ print(amazon_cart2)
 # Matrix
 
 matrix = [
-    [1,2,3],
-    [2,4,6],
-    [7,8,9],
+    [1, 2, 3],
+    [2, 4, 6],
+    [7, 8, 9],
 ]
 # matrix[0][1] = 2
 
-basket = [1,2,3,4,5]
+basket = [1, 2, 3, 4, 5]
 
 # adding
 # new_list = basket.append(100)
 basket.append(100)
-basket.insert(3,100)
+basket.insert(3, 100)
 basket.extend([200, 300])
 # print(new_list)
 print(basket)
@@ -174,7 +175,7 @@ print(basket)
 basket.remove(200)
 print(basket)
 
-basket2 = [1,2,3,4,5]
+basket2 = [1, 2, 3, 4, 5]
 new_basket2 = basket2.pop(1)
 print(new_basket2)
 
@@ -212,10 +213,135 @@ print(new_sentence)
 
 # list unpacking
 
-a,b,c, *other, d = [1,2,3,4,5,6,7,8,9]
+a, b, c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 print(a)
 print(b)
 print(c)
 print(d)
 print(other)
+
+# Dictionary
+
+dictionary = {
+    'a': [1, 2, 3],
+    'b': 'hello',
+    'x': True
+}
+
+my_list = [
+    {
+        'a': [1, 2, 3],
+        'b': 'hello',
+        'x': True
+    },
+    {
+        'a': [4, 5, 6],
+        'b': 'hello',
+        'x': True
+    }
+]
+
+print(my_list[0]['a'][2])
+print(dictionary['a'][1])
+
+# Dictionary Keys
+
+dictionary = {
+    123: [1, 2, 3],
+    True: 'hello',
+    # [100]: True
+}
+
+print(dictionary[123])
+print(dictionary[True])
+# print(dictionary[[100]])
+
+user = {
+    'basket': [1,2,3],
+    'greet': 'hello',
+    'age': 57
+}
+
+print(user.get('age'))
+print(user.get('age', 55))
+
+user2 = dict(name='Peter')
+print(user2)
+
+print('basket' in user.keys())
+print('hello' in user.values())
+print(user.items())
+print('bas' in user.items())
+print([1,2,3] in user.items())
+print(57 in user.items())
+
+user3 = user.copy()
+user.clear()
+print(user)
+print(user3)
+print(user3.pop('age'))
+print(user3)
+print(user3.popitem())
+print(user3)
+print(user3.update({'basket': [4,5,6]}))
+print(user3)
+
+# Tuple
+
+my_tuple = (1,2,3,4,5)
+print(my_tuple[2])
+print(5 in my_tuple)
+
+new_tuple = my_tuple[1:2]
+print(new_tuple)
+new_tuple2 = my_tuple[1:4]
+print(new_tuple2)
+
+x,y,z, *other = (1,2,3,4,5)
+print(x)
+print(y)
+print(z)
+print(other)
+
+print(my_tuple.count(3))
+print(my_tuple.index(3))
+print(len(my_tuple))
+
+# Set
+
+my_set = {1,2,3,4,5,5}
+my_set.add(100)
+my_set.add(2)
+print(my_set)
+
+my_temp_list = [1,2,3,4,5,5]
+
+print(set(my_temp_list))
+
+print(1 in my_set)
+print(len(my_set))
+print(list(my_set))
+
+new_set = my_set.copy()
+print(new_set)
+my_set.clear()
+print(my_set)
+
+set_one = {1,2,3,4,5}
+set_two = {4,5,6,7,8,9,10}
+set_three = {6,7}
+
+print('set methods')
+# print(set_one.difference(set_two))
+# print(set_one.discard(5))
+# print(set_one)
+# print(set_one.difference_update(set_two))
+# print(set_one)
+print(set_one.intersection(set_two))
+print(set_one & set_two)
+print(set_one.isdisjoint(set_two))
+print(set_three.issubset(set_two))
+print(set_two.issuperset(set_three))
+print(set_one.union(set_two))
+print(set_one | set_two)
