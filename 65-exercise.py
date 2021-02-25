5d89a148478a42bc83bfa2c54994a225ed22ef84
petercolella <peter.joseph.colella@gmail.com>
2021-02-24 21:41:46 -0500

    65 second try

--- 65-exercise.py
+++ 65-exercise.py
@@ -1,5 +1,5 @@
 is_magician = True
-is_expert = False
+is_expert = True
 
 # check if magician AND expert: "you are a master magician"
 
@@ -7,11 +7,19 @@ is_expert = False
 
 # if you're not a magicain: "You need magic powers"
 
-if is_magician and is_expert:
-    print('you are a master magician')
+# if is_magician and is_expert:
+#     print('you are a master magician')
 
-if is_magician and not is_expert:
-    print('at least you\'re getting there')
+# if is_magician and not is_expert:
+#     print('at least you\'re getting there')
 
-if not is_magician:
+# if not is_magician:
+#     print('you need magic powers')
+
+if is_magician:
+    if is_expert:
+        print('you are a master magician')
+    else:
+        print('at least you\'re getting there')
+else:
     print('you need magic powers')
