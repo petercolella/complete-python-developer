de5d82f31ec85d10eacffa29e7e7755029e82b4a
petercolella <peter.joseph.colella@gmail.com>
2021-02-25 19:52:15 -0500

    66 - 71

--- main.py
+++ main.py
@@ -33,3 +33,76 @@ print(1 < 2 >3 < 4)
 
 print(not(True))
 print(not(1 != 1))
+
+print('---66---')
+print(True == 1) # False incorrect
+print('' == 1) # False 
+print([] == 1) # False
+print(10 == 10.0) # False incorrect
+print([] == []) # False incorrect
+print('------')
+print(True is 1) # False
+print('' is 1) # False 
+print([] is 1) # False
+print(10 is 10) # True
+print(10 is 10.0) # False
+print([] is []) # False
+
+for item in 'Zero to Mastery':
+    print(item)
+for item in [1,2,3,4,5]: # {1,2,3,4,5} and (1,2,3,4,5) same
+    print(item)
+
+# iterable - list, dictionary, tuple, set, string
+
+user = {
+    'name': "Golelm",
+    'age': 5006,
+    'can_swim': False
+}
+
+for key in user:
+    print(key)
+
+for thing in user.items():
+    print(thing)
+for thing in user.values():
+    print(thing)
+for thing in user.keys():
+    print(thing)
+for thing in user.items():
+    key, value = thing
+    print(thing)
+for key, value in user.items():
+    print(key, value)
+
+# counter
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+print(sum(my_list))
+
+counter = 0
+for item in my_list:
+    counter = counter + item
+print(counter)
+
+# range
+
+print(range(100))
+
+for _ in range(10):
+    print(_)
+for _ in range(0,10,2):
+    print(_)
+for _ in range(10,0,-2):
+    print(_)
+print(list(range(10)))
+
+# enumerate
+
+for i,char in enumerate('Helllooo'):
+    print(i, char)
+
+for i,char in enumerate(list(range(100))):
+    if char == 50:
+        print(i)
