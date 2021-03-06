99d5a07ea9f7dd576a2445bd1427453213642a35
petercolella <peter.joseph.colella@gmail.com>
2021-03-05 22:23:26 -0500

    77-exercise.py third solution

--- 77-exercise.py
+++ 77-exercise.py
@@ -6,7 +6,9 @@ some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 #         temp_list.append(element)
 # print(temp_list)
 
-temp_list = some_list.copy()
-for el in set(some_list):
-    temp_list.remove(el)
-print(temp_list)
+# temp_list = some_list.copy()
+# for el in set(some_list):
+#     temp_list.remove(el)
+# print(temp_list)
+
+print([el for el in set(some_list) if some_list.count(el) > 1])
