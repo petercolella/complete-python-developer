2abd0112aebaaa1bb087ddfd0a5b90f01100b4e0
petercolella <peter.joseph.colella@gmail.com>
2021-03-05 21:09:15 -0500

    77-exercise.py first solution

---
+++ 77-exercise.py
@@ -0,0 +1,8 @@
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+temp_list = []
+for element in some_list:
+    if some_list.count(element) > 1 and temp_list.count(element) == 0:
+        temp_list.append(element)
+
+print(temp_list)
