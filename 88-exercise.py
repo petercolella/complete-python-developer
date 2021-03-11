a2aaf8be4223dc0d5bfc2beb621fb37c3c07170c
petercolella <peter.joseph.colella@gmail.com>
2021-03-10 20:37:05 -0500

    88 official solution

--- 88-exercise.py
+++ 88-exercise.py
@@ -13,3 +13,19 @@ print(highest_even([10,2,3,4,8,11]))
 print(highest_even([-10,-2,-3,-4,-8,-11]))
 print(highest_even([3,5,11]))
 print(highest_even([3,5,11,2]))
+print(highest_even([]))
+
+# Official solution which breaks with empty list or all odd numbers
+
+def highest_even_official(li):
+    evens = []
+    for item in li:
+        if item % 2 == 0:
+            evens.append(item)
+    return max(evens)
+
+print(highest_even_official([10,2,3,4,8,11]))
+print(highest_even_official([-10,-2,-3,-4,-8,-11]))
+print(highest_even_official([3,5,11]))
+print(highest_even_official([3,5,11,2]))
+print(highest_even([]))
