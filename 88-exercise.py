58407255d354617f99128f34654d1063033013c8
petercolella <peter.joseph.colella@gmail.com>
2021-03-10 20:25:32 -0500

    88 second solution

--- 88-exercise.py
+++ 88-exercise.py
@@ -1,9 +1,15 @@
 def highest_even(li):
-    result = 0
+    result = None
     for value in li:
         if value % 2 == 0:
+            if not result:
+                result = value
+                continue
             if value > result:
                 result = value
     return result
 
 print(highest_even([10,2,3,4,8,11]))
+print(highest_even([-10,-2,-3,-4,-8,-11]))
+print(highest_even([3,5,11]))
+print(highest_even([3,5,11,2]))
