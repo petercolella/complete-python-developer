648ba5b01fd378462c0f92ed4bf5551841e18af9
petercolella <peter.joseph.colella@gmail.com>
2021-03-17 22:42:43 -0400

    119. Exercise: Cats Everywhere refactored solution

--- 119-exercise.py
+++ 119-exercise.py
@@ -10,21 +10,12 @@ cat1 = Cat('Felix', 2)
 cat2 = Cat('Garfield', 6)
 cat3 = Cat('Morris', 10)
 
-# print(cat1, cat2, cat3)
-
 
 # Create a function that finds the oldest cat
-cats = [cat1, cat2, cat3]
-
-# print(cats[1:])
-
-def findOldestCat(catList):
-    oldestCat = catList[0]
-    for cat in catList[1:]:
-        if (cat.age > oldestCat.age):
-            oldestCat = cat
-    return oldestCat
+def findOldestCat(*cats):
+    from operator import attrgetter
+    return max(cats, key=attrgetter('age'))
 
 
 # Print our: "The oldest cat is x years old.". x will be the oldest cat's age by using the function in #2.
-print(f'The oldest cat is {findOldestCat(cats).age} years old.')
+print(f'The oldest cat is {findOldestCat(cat1, cat2, cat3).age} years old.')
