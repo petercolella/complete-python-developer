c9c84aba2af8bc19a8a32e2aff3cdbaa04f1608b
petercolella <peter.joseph.colella@gmail.com>
2021-04-20 22:34:05 -0400

    146 - 147

--- main.py
+++ main.py
@@ -55,3 +55,38 @@ print(reduce(accumulator, my_list, 0))
 print(list(map(lambda item: item*2, my_list)))
 print(list(filter(lambda item: item % 2 != 0, my_list)))
 print(reduce(lambda acc, item: acc + item, my_list))
+
+# 146. List Comprehensions
+# list, set, dictionary
+
+my_list_comp = [char for char in 'hello']
+my_list_comp2 = [num for num in range(0, 100)]
+my_list_comp3 = [num*2 for num in range(0, 100)]
+my_list_comp4 = [num**2 for num in range(0, 100) if num % 2 == 0]
+
+# for char in 'hello':
+#     my_list_comp.append(char)
+
+print(my_list_comp)
+print(my_list_comp2)
+print(my_list_comp3)
+print(my_list_comp4)
+
+# 147. Set and Dictionary Comprehension
+
+my_set = {char for char in 'hello'}
+my_set2 = {num for num in range(0, 100)}
+
+print(my_set)
+print(my_set2)
+
+simple_dict = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+}
+my_dict = {k: v**2 for k, v in simple_dict.items() if v % 2 == 0}
+my_dict2 = {num: num*2 for num in [1, 2, 3]}
+
+print(my_dict)
+print(my_dict2)
