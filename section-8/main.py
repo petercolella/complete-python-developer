5836af7f170971bde83b3123529a1dbe4ef778ef
petercolella <peter.joseph.colella@gmail.com>
2021-04-26 20:40:55 -0400

    150 - 153

---
+++ section-8/main.py
@@ -0,0 +1,92 @@
+# 150. Decorators
+
+def hello():
+    print('hello')
+
+
+greet = hello
+del hello
+
+greet()
+
+# hello()
+
+
+def hello_again(func):  # Higher Ordr Function HOC
+    func()
+
+
+def greet_again():
+    print('still here')
+
+
+hello_again(greet_again)
+print(hello_again(greet_again))
+
+# 151. Higher Order Functions
+
+
+def greet2():
+    def func():
+        return 5
+    return func
+
+# 152 Decorators 2
+
+# def hello_a_third_freaking_time():
+#     print('hello')
+
+
+def my_decorator(func):
+    def wrap_func():
+        print('******')
+        func()
+        print('******')
+    return wrap_func
+
+
+@my_decorator
+def hello_a_third_freaking_time():
+    print('hello')
+
+
+@my_decorator
+def bye():
+    print('see ya later')
+
+
+hello_a_third_freaking_time()
+bye()
+
+print('2-----2')
+
+hello_number_four = my_decorator(hello_a_third_freaking_time)
+hello_number_four()
+
+# 153. Decorators 3
+
+print('3-----3')
+
+
+def my_decorator_two(func):
+    def wrap_func(*args, **kwargs):
+        print('******')
+        func(*args, **kwargs)
+        print('******')
+    return wrap_func
+
+
+@my_decorator_two
+def hello_with_greeting(greeting):
+    print(greeting)
+
+
+@my_decorator_two
+def hello_with_greeting_emoji(greeting, emoji):
+    print(greeting, emoji)
+
+
+print('4-----4')
+
+hello_with_greeting('hiii')
+hello_with_greeting_emoji('hiii', ':)')
