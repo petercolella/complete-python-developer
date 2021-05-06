68c81979f527108e2e088c03d3d903453d739b24
petercolella <peter.joseph.colella@gmail.com>
2021-05-05 22:00:42 -0400

    165. Exercise: Fibonacci Numbers - my solution

---
+++ section-10/165-exercise.py
@@ -0,0 +1,33 @@
+# 165. Exercise: Fibonacci Numbers
+
+def fib_gen(number):
+    current = 1
+    previous = 0
+    for i in range(number + 1):
+        if i == 0 or i == 1:
+            yield i
+        else:
+            yield current + previous
+            temp = current
+            current = current + previous
+            previous = temp
+
+
+def fib_func(number):
+    for item in fib_gen(number):
+        print(item)
+
+
+fib_func(20)
+
+print('--------------------')
+
+
+def fib_func_list(number):
+    result = []
+    for item in fib_gen(number):
+        result.append(item)
+    print(result)
+
+
+fib_func_list(20)
